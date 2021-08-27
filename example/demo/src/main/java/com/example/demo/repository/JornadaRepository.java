1acd14a2f5f06640a0f65b8ec3b526e4e690ab19
KarenCamp <karen-campinas@hotmail.com>
2021-08-26 22:38:55 -0300

    first commit

---
+++ example/demo/src/main/java/com/example/demo/repository/JornadaRepository.java
@@ -0,0 +1,8 @@
+package com.example.demo.repository;
+import com.example.demo.model.JornadaTrabalho;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JornadaRepository extends JpaRepository<JornadaTrabalho,Long> {
+}
